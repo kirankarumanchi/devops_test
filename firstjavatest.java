b6f3fc6dcbebcb4d30e1dee4484c465f21f3bac0
kirankarumanchi <31979628+kirankarumanchi@users.noreply.github.com>
2018-02-20 10:47:07 +1000

    Added my first Java file to the repository

---
+++ firstjavatest.java
@@ -0,0 +1,14 @@
+import java.lang.*
+
+        public class firstjavatest
+        {
+            firstjavatest()
+            {
+                System.out.println("Hello");
+            }
+        }
+
+        public static void main(String []args)
+        {
+            firstjavatest f1 = new firstjavatest();
+        }
